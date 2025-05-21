45666623647ffff540f5d784da3423656616b8dc
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2025-05-21 01:50:41 +0000

    Intégration du template principal

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function adjustMainMargin() {
+    const header = document.getElementById("gHeader");
+    const main = document.getElementById("gMain");
+    if (header && main) {
+        const headerHeight = header.offsetHeight;
+        main.style.marginTop = headerHeight + 20 + "px";
+    }
+}
+
+// Ajuste au chargement de la page
+window.addEventListener("load", adjustMainMargin);
+
+// Réajuste si la fenêtre est redimensionnée
+window.addEventListener("resize", adjustMainMargin);
