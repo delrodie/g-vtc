9e9f2df8d01472f5ddea5b6a52010a60ba07b7e6
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2025-05-22 02:44:08 +0000

    Generation de l'entite CHAUFFEUR

---
+++ src/Entity/Chauffeur.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChauffeurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ChauffeurRepository::class)]
+class Chauffeur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $matricule = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $telephone = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $permis = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMatricule(): ?string
+    {
+        return $this->matricule;
+    }
+
+    public function setMatricule(?string $matricule): static
+    {
+        $this->matricule = $matricule;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(?string $telephone): static
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getPermis(): ?string
+    {
+        return $this->permis;
+    }
+
+    public function setPermis(?string $permis): static
+    {
+        $this->permis = $permis;
+
+        return $this;
+    }
+}
