c247947f4de8023a319092d09b3253bb63909bd3
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2025-05-31 11:28:34 +0000

    Accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,15 +4,30 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Service\RepositoriesService;
+use App\Service\UtilityService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name:'app_home')]
-    public function index(): Response
+    public function __construct(
+        private RepositoriesService $repositoriesService,
+        private UtilityService $utilityService
+    )
     {
-        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/', name:'app_home')]
+    public function index(Request $request): Response
+    {
+        $periode = $this->utilityService->periode($request);
+        return $this->render('home/index.html.twig',[
+            'recette_mois' => $this->repositoriesService->getMontantByTypeAndPeriode(UtilityService::ENTREE, $periode['dateDebut'], $periode['dateFin']),
+            'depense_mois' => $this->repositoriesService->getMontantByTypeAndPeriode(UtilityService::SORTIE, $periode['dateDebut'], $periode['dateFin']),
+            'vehicules' => $this->repositoriesService->getAllVehiculeOccupe()
+        ]);
     }
 }
